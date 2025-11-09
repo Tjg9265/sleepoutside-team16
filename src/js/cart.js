978d4eb602e4044ebb0dad3c39dca3c871a2d568
RommelJuarez <cap03487@gmail.com>
2025-11-08 22:31:19 -0500

    Refactor cart item template for improved readability and consistency

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,36 +1,44 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const itemsArray = Array.isArray(cartItems) ? cartItems : [cartItems];
   const htmlItems = itemsArray.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
-
   document.querySelectorAll(".cart-card__remove").forEach((btn) => {
     btn.addEventListener("click", removeItemFromCart);
   });
 }
 
-
 function cartItemTemplate(item) {
-  return `
-    <li class="cart-card divider">
-      <span class="cart-card__remove" data-id="${item.Id}">✖</span>
-      <a href="#" class="cart-card__image">
-        <img src="${item.Image}" alt="${item.Name}" />
-      </a>
-      <a href="#">
-        <h2 class="card__name">${item.Name}</h2>
-      </a>
-      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-      <p class="cart-card__quantity">qty: 1</p>
-      <p class="cart-card__price">$${item.FinalPrice}</p>
-    </li>
-  `;
+  return (
+    "<li class='cart-card divider'>" +
+    "<span class='cart-card__remove' data-id='" +
+    item.Id +
+    "'>✖</span>" +
+    "<a href='#' class='cart-card__image'>" +
+    "<img src='" +
+    item.Image +
+    "' alt='" +
+    item.Name +
+    "' />" +
+    "</a>" +
+    "<a href='#'>" +
+    "<h2 class='card__name'>" +
+    item.Name +
+    "</h2>" +
+    "</a>" +
+    "<p class='cart-card__color'>" +
+    item.Colors[0].ColorName +
+    "</p>" +
+    "<p class='cart-card__quantity'>qty: 1</p>" +
+    "<p class='cart-card__price'>$" +
+    item.FinalPrice +
+    "</p>" +
+    "</li>"
+  );
 }
-
 
 function removeItemFromCart(event) {
   const productId = event.target.dataset.id;
@@ -41,6 +49,5 @@ function removeItemFromCart(event) {
   setLocalStorage("so-cart", updatedCart);
   renderCartContents();
 }
-
 
 renderCartContents();
